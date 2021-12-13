79334a79f4f1503c4d878cc6df2cdf254a14d122
Jan Golebiowski <bowski23@hotmail.de>
2021-12-13 16:03:19 +0100

    chart test

--- lib/pages/sub_pages/overview_page.dart
+++ lib/pages/sub_pages/overview_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import '../../ui/week_view.dart';
 
 class OverviewPage extends StatefulWidget {
   OverviewPage({@required this.counter, Key key, this.title}) : super(key: key);
@@ -20,7 +21,9 @@ class _OverviewPageState extends State<OverviewPage> {
   @override
   Widget build(BuildContext context) {
     var val = counter.value.toString();
-    var textChildren = <Widget>[
+
+    //add text children
+    var children = <Widget>[
       Text(
         'Du hast bisher',
         style: Theme.of(context).textTheme.headline6,
@@ -34,12 +37,17 @@ class _OverviewPageState extends State<OverviewPage> {
         style: Theme.of(context).textTheme.headline6,
       ),
     ];
+
+    //add weekView
+    var weekView = WeekViewChart();
+    children.add(weekView);
+
     return Container(
       alignment: Alignment.topCenter,
       padding: EdgeInsets.all(16),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
-        children: textChildren,
+        children: children,
       ),
     ); // This trailing comma makes auto-formatting nicer for build methods.
   }
