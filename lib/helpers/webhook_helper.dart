ce103c7f40a1c56b20d6a520507404c584ef4284
Jan Golebiowski <9009766+bowski23@users.noreply.github.com>
2023-01-23 10:32:59 +0100

    faas interface

--- lib/helpers/webhook_helper.dart
+++ lib/helpers/webhook_helper.dart
@@ -5,7 +5,6 @@ import 'package:http/http.dart';
 
 Future<void> publishToDiscord(BeerEntry entry) async {
   var name = Settings.publishToDiscordName.value;
-  await post(Uri.parse(discordHook),
-      headers: {"content-type": "application/json"},
-      body: "{\"content\":\"$name hat ein Bier der Marke: ${entry.brand} getrunken.\",\"tts\":\"true\"}");
+  var result = await post(Uri.parse(azurePublishFunction),
+      headers: {"content-type": "application/json"}, body: "{\"name\":\"$name\", \"brand\": \"${entry.brand}\"}");
 }
